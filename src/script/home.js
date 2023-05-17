71462eac66980f8e18eb70bed5d80bf511f06032
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-05-17 19:40:52 +0530

    hero slider

--- src/script/home.js
+++ src/script/home.js
@@ -4,3 +4,14 @@ document.querySelector("#Footer").innerHTML = footer();
 
 document.querySelector("#navBar").innerHTML = navbar();
 
+
+
+let arr=[
+   "../img/banenr-img1.jpg",
+   "../img/banenr-img1.jpg",
+   "../img/banenr-img1.jpg"
+]
+
+    window.addEventListener("load", (event) => {
+        console.log("object");
+        });
