f56427c434d7910876f39004c49e3f22811b8031
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-05-18 13:23:43 +0530

    Menu

--- src/script/Menu.js
+++ src/script/Menu.js
@@ -4,3 +4,68 @@ document.querySelector("#Footer").innerHTML = footer();
 
 document.querySelector("#navBar").innerHTML = navbar();
 
+let arr = [
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter2.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter3.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+  {
+    img: "../img/starter1.jpg",
+    text: "Panner Tikka",
+    category:["Fresh","Spicy","India"],
+    price:"80",
+    
+  },
+];
