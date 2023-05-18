6264bf77de373b18f4bb2291b3c735e4da818247
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-05-18 13:36:38 +0530

    Menu Array done

--- src/script/Menu.js
+++ src/script/Menu.js
@@ -8,64 +8,64 @@ let arr = [
   {
     img: "../img/starter1.jpg",
     text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
+    category:["Crispy","Spicy","India"],
     price:"80",
     
   },
   {
     img: "../img/starter2.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
+    text: "Malai Kofta",
+    category:["Soft","Sweet","India"],
+    price:"90",
     
   },
   {
     img: "../img/starter3.jpg",
-    text: "Panner Tikka",
+    text: "Fish Fry",
     category:["Fresh","Spicy","India"],
+    price:"110",
+    
+  },
+  {
+    img: "../img/maincourse1.jpg",
+    text: "Butter Chicken",
+    category:["Fresh","Medium","India"],
+    price:"140",
+    
+  },
+  {
+    img: "../img/maincourse2.jpg",
+    text: "Biryani",
+    category:["Fresh","Spicy","India"],
+    price:"150",
+    
+  },
+  {
+    img: "../img/maincourse3.jpg",
+    text: "Palak Paneer",
+    category:["Fresh","Sweet","India"],
+    price:"120",
+    
+  },
+  {
+    img: "../img/Dessert1.jpg",
+    text: "Ice Cream",
+    category:["Fresh","Sweet","India"],
+    price:"40",
+    
+  },
+  {
+    img: "../img/Dessert2.jpg",
+    text: "Pastry",
+    category:["Fresh","Sweet","India"],
     price:"80",
     
   },
   {
-    img: "../img/starter1.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
-    
-  },
-  {
-    img: "../img/starter1.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
-    
-  },
-  {
-    img: "../img/starter1.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
-    
-  },
-  {
-    img: "../img/starter1.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
-    
-  },
-  {
-    img: "../img/starter1.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
-    
-  },
-  {
-    img: "../img/starter1.jpg",
-    text: "Panner Tikka",
-    category:["Fresh","Spicy","India"],
-    price:"80",
+    img: "../img/Dessert3.jpg",
+    text: "Milk Shake",
+    category:["Fresh","Sweet","India"],
+    price:"50",
     
   },
 ];
