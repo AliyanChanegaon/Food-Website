56b48a79f53cd2a2d9602abaa157fa690527fa5c
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-05-18 14:07:21 +0530

    Filter Menu

--- src/script/Menu.js
+++ src/script/Menu.js
@@ -4,8 +4,15 @@ document.querySelector("#Footer").innerHTML = footer();
 
 document.querySelector("#navBar").innerHTML = navbar();
 
+let container= document.querySelector('.container');
+const allProduct=document.querySelector('.filters >div:nth-child(1)');
+const Starters=document.querySelector('.filters >div:nth-child(2)');
+const Mains=document.querySelector('.filters >div:nth-child(3)');
+const Dessert=document.querySelector('.filters >div:nth-child(4)');
+
 let arr = [
   {
+    type:"Starters",
     img: "../img/starter1.jpg",
     text: "Panner Tikka",
     category:["Crispy","Spicy","India"],
@@ -13,6 +20,7 @@ let arr = [
     
   },
   {
+    type:"Starters",
     img: "../img/starter2.jpg",
     text: "Malai Kofta",
     category:["Soft","Sweet","India"],
@@ -20,6 +28,7 @@ let arr = [
     
   },
   {
+    type:"Starters",
     img: "../img/starter3.jpg",
     text: "Fish Fry",
     category:["Fresh","Spicy","India"],
@@ -27,6 +36,7 @@ let arr = [
     
   },
   {
+    type:"Mains",
     img: "../img/maincourse1.jpg",
     text: "Butter Chicken",
     category:["Fresh","Medium","India"],
@@ -34,6 +44,7 @@ let arr = [
     
   },
   {
+    type:"Mains",
     img: "../img/maincourse2.jpg",
     text: "Biryani",
     category:["Fresh","Spicy","India"],
@@ -41,6 +52,7 @@ let arr = [
     
   },
   {
+    type:"Mains",
     img: "../img/maincourse3.jpg",
     text: "Palak Paneer",
     category:["Fresh","Sweet","India"],
@@ -48,6 +60,7 @@ let arr = [
     
   },
   {
+    type:"Dessert",
     img: "../img/Dessert1.jpg",
     text: "Ice Cream",
     category:["Fresh","Sweet","India"],
@@ -55,6 +68,7 @@ let arr = [
     
   },
   {
+    type:"Dessert",
     img: "../img/Dessert2.jpg",
     text: "Pastry",
     category:["Fresh","Sweet","India"],
@@ -62,6 +76,7 @@ let arr = [
     
   },
   {
+    type:"Dessert",
     img: "../img/Dessert3.jpg",
     text: "Milk Shake",
     category:["Fresh","Sweet","India"],
@@ -69,3 +84,36 @@ let arr = [
     
   },
 ];
+
+
+Starters.addEventListener("click", filteredRole);
+
+
+
+
+function filteredRole(type) {
+ 
+  let filterArr = arr.filter(function (el) {
+    return el.type == type;
+  });
+
+  displayData(filterArr);
+}
+
+const displayData= ()=>{
+    arr.forEach((ele,index)=>{
+        container.innerHTML+=
+        `<div class="box">
+        <img src=${ele.img} alt="">
+        <h1>${ele.text}</h1>
+        <span>${ele.category[0]} <span>.</span> <span>${ele.category[1]}</span><span>.</span>${ele.category[2]}</span>
+        <div>₹${ele.price}</div>
+        <!-- <div id="buy-hover">Add to Cart</div> -->
+      </div>`;
+
+      
+
+    })
+}
+
+displayData();
